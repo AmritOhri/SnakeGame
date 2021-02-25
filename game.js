6db7ba875dc36ecaccbf366dc2c0e32867493723
Amrit Ohri <amritohri2000@gmail.com>
2021-02-26 01:54:59 +0530

    Game complete

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
-import {update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
-
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
 let lastRenderTime=0;
+let gameOver = false
+import {outsideGrid} from './grid.js'
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime){
+
+    if(gameOver){
+        if(confirm('You lost. Press OK to restart.')) {
+            window.location = '/'
+        }
+        return
+    }
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender=(currentTime - lastRenderTime) / 1000
     if(secondsSinceLastRender < 1/SNAKE_SPEED) return 
@@ -18,8 +28,15 @@ window.requestAnimationFrame(main)
 
 function update(){
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw(){
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath(){
+    gameOver = (outsideGrid(getSnakeHead()) || snakeIntersection() )
 }
